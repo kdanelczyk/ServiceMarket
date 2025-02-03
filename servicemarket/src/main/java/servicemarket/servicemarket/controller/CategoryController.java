a2bf2fe35a025d1e129de4a54994f3b48e08470a
Kamil <kamil.danelczyk@gmail.com>
2025-02-03 22:21:38 +0100

    Add controllers

---
+++ servicemarket/src/main/java/servicemarket/servicemarket/controller/CategoryController.java
@@ -0,0 +1,68 @@
+package servicemarket.servicemarket.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import servicemarket.servicemarket.model.Category;
+import servicemarket.servicemarket.model.InputDto.CategoryInput;
+import servicemarket.servicemarket.service.CategoryService;
+
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin(origins = { "http://localhost:80", "http://frontend-service:80", "http://frontend-service:3000" })
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/page")
+    public ResponseEntity<PageImpl<Category>> getCategories(
+            @RequestParam int page,
+            @RequestParam int size) {
+        Page<Category> categories = categoryService.getAllCategories(page, size);
+        return ResponseEntity
+                .ok(new PageImpl<>(categories.getContent(), categories.getPageable(), categories.getTotalElements()));
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
+    public ResponseEntity<Category> getCategoryById(@PathVariable String id) {
+        return ResponseEntity.ok(categoryService.getCategoryById(id));
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN','SUPER_ADMIN')")
+    public ResponseEntity<Category> createCategory(@Valid @RequestBody CategoryInput categoryInput) {
+        return ResponseEntity.ok(categoryService.createCategory(categoryInput));
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
+    public ResponseEntity<Category> updateCategory(
+            @PathVariable String id,
+            @Valid @RequestBody CategoryInput categoryInput) {
+        return ResponseEntity.ok(categoryService.updateCategory(id, categoryInput));
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','SUPER_ADMIN')")
+    public ResponseEntity<Void> deleteCategory(@PathVariable String id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ servicemarket/src/main/java/servicemarket/servicemarket/controller/TaskController.java
@@ -0,0 +1,146 @@
+package servicemarket.servicemarket.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import servicemarket.servicemarket.model.Task;
+import servicemarket.servicemarket.model.TaskOffer;
+import servicemarket.servicemarket.model.TaskRequest;
+import servicemarket.servicemarket.model.InputDto.TaskOfferInput;
+import servicemarket.servicemarket.model.InputDto.TaskRequestInput;
+import servicemarket.servicemarket.service.TaskService;
+
+@RestController
+@RequestMapping("/tasks")
+@CrossOrigin(origins = { "http://localhost:80", "http://frontend-service:80", "http://localhost:3000", })
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("/offers/page")
+    public ResponseEntity<PageImpl<TaskOffer>> getAllTaskOffers(
+            @RequestParam int page,
+            @RequestParam int size) {
+        Page<TaskOffer> taskOffers = taskService.getAllTasksOffers(page, size);
+        return ResponseEntity
+                .ok(new PageImpl<>(taskOffers.getContent(), taskOffers.getPageable(), taskOffers.getTotalElements()));
+    }
+
+    @GetMapping("/requests/page")
+    public ResponseEntity<PageImpl<TaskRequest>> getAllTaskRequests(
+            @RequestParam int page,
+            @RequestParam int size) {
+        Page<TaskRequest> taskRequests = taskService.getAllTasksRequests(page, size);
+        return ResponseEntity.ok(
+                new PageImpl<>(taskRequests.getContent(), taskRequests.getPageable(), taskRequests.getTotalElements()));
+    }
+
+    @GetMapping("/requests/category/{categoryId}/page")
+    public ResponseEntity<PageImpl<TaskRequest>> getTaskRequestsByCategoryId(
+            @PathVariable String categoryId,
+            @RequestParam int page,
+            @RequestParam int size) {
+        Page<TaskRequest> taskRequests = taskService.getTaskRequestsByCategoryId(categoryId, page, size);
+        return ResponseEntity.ok(
+                new PageImpl<>(taskRequests.getContent(), taskRequests.getPageable(), taskRequests.getTotalElements()));
+    }
+
+    @GetMapping("/offers/category/{categoryId}/page")
+    public ResponseEntity<PageImpl<TaskOffer>> getTaskOffersByCategoryId(
+            @PathVariable String categoryId,
+            @RequestParam int page,
+            @RequestParam int size) {
+        Page<TaskOffer> taskOffers = taskService.getTaskOffersByCategoryId(categoryId, page, size);
+        return ResponseEntity
+                .ok(new PageImpl<>(taskOffers.getContent(), taskOffers.getPageable(), taskOffers.getTotalElements()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable String id) {
+        return ResponseEntity.ok(taskService.getTaskById(id));
+    }
+
+    @GetMapping("/info/loggedIn/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPER_ADMIN')")
+    public ResponseEntity<Task> getInfoAbout(@PathVariable String id) {
+        taskService.getInfoAboutIfLoggedIn(id);
+        return ResponseEntity.ok(taskService.getTaskById(id));
+    }
+
+    @GetMapping("/info/notLoggedIn/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPER_ADMIN')")
+    public ResponseEntity<Task> getInfoAbout(
+            @PathVariable String id,
+            @PathVariable String name,
+            @PathVariable String email) {
+        taskService.getInfoAboutIfNotLoggedIn(id, name, email);
+        return ResponseEntity.ok(taskService.getTaskById(id));
+    }
+
+    @PostMapping("/requests/category/{categoryId}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPER_ADMIN')")
+    public ResponseEntity<Task> createTaskRequest(@PathVariable String categoryId,
+            @Valid @ModelAttribute TaskRequestInput taskRequestInput) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(taskService.createTaskRequest(taskRequestInput, categoryId));
+    }
+
+    @PostMapping("/offers/category/{categoryId}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPER_ADMIN')")
+    public ResponseEntity<Task> createTaskOffer(@PathVariable String categoryId,
+            @Valid @ModelAttribute TaskOfferInput taskOfferInput) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTaskOffer(taskOfferInput, categoryId));
+    }
+
+    @PutMapping("/requests/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPER_ADMIN')")
+    public ResponseEntity<Task> updateTaskRequest(@PathVariable String id,
+            @Valid @ModelAttribute TaskRequestInput taskRequestInput) {
+        return ResponseEntity.ok(taskService.updateTask(id, taskRequestInput));
+    }
+
+    @PutMapping("/offers/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPER_ADMIN')")
+    public ResponseEntity<Task> updateTaskOffer(@PathVariable String id,
+            @Valid @ModelAttribute TaskOfferInput taskOfferInput) {
+        return ResponseEntity.ok(taskService.updateTask(id, taskOfferInput));
+    }
+
+    @GetMapping("/image/{taskId}/{index}")
+    public ResponseEntity<byte[]> getImage(
+            @PathVariable String taskId,
+            @PathVariable int index) {
+        try {
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(taskService.getTaskImage(taskId, index));
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPER_ADMIN')")
+    public ResponseEntity<Void> deleteTask(@PathVariable String id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
