9c3e346ca58d1181ddda93cf3acbf5b0d53d67f8
Kamil <kamil.danelczyk@gmail.com>
2025-02-03 20:50:05 +0100

    Add senderservice with dependencies

---
+++ senderservice/src/main/java/senderservice/senderservice/SenderserviceApplication.java
@@ -0,0 +1,13 @@
+package senderservice.senderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenderserviceApplication.class, args);
+	}
+
+}
---
+++ senderservice/src/test/java/senderservice/senderservice/SenderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package senderservice.senderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
