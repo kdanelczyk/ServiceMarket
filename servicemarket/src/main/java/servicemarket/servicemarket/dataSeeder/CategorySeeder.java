3382dce8b1afd15390e5495fe566daa6c18967bd
Kamil <kamil.danelczyk@gmail.com>
2025-02-04 21:43:49 +0100

    Code formatting

--- servicemarket/src/main/java/servicemarket/servicemarket/dataSeeder/CategorySeeder.java
+++ servicemarket/src/main/java/servicemarket/servicemarket/dataSeeder/CategorySeeder.java
@@ -7,90 +7,92 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.annotation.Order;
 import org.springframework.lang.NonNull;
 
 import servicemarket.servicemarket.model.Category;
 import servicemarket.servicemarket.repository.CategoryRepository;
 
 @Configuration
+@Order(1)
 public class CategorySeeder implements ApplicationListener<ContextRefreshedEvent> {
 
-    @Autowired
-    private CategoryRepository categoryRepository;
+        @Autowired
+        private CategoryRepository categoryRepository;
 
-    @Override
-    public void onApplicationEvent(@NonNull ContextRefreshedEvent event) {
-        categoryRepository.deleteAll();
-        List<Category> categories = List.of(
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Carpentry")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Gardening")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Construction")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Mechanics")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Tailoring")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Electrical Work")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Photography")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Plumbing")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Transportation")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Cosmetology")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Painting")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Gastronomy")
-                        .tasks(List.of())
-                        .build(),
-                Category.builder()
-                        .id(UUID.randomUUID().toString())
-                        .name("Translation Services")
-                        .tasks(List.of())
-                        .build());
+        @Override
+        public void onApplicationEvent(@NonNull ContextRefreshedEvent event) {
+                categoryRepository.deleteAll();
+                List<Category> categories = List.of(
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Carpentry")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Gardening")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Construction")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Mechanics")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Tailoring")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Electrical Work")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Photography")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Plumbing")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Transportation")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Cosmetology")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Painting")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Gastronomy")
+                                                .tasks(List.of())
+                                                .build(),
+                                Category.builder()
+                                                .id(UUID.randomUUID().toString())
+                                                .name("Translation Services")
+                                                .tasks(List.of())
+                                                .build());
 
-        categoryRepository.saveAll(categories);
+                categoryRepository.saveAll(categories);
 
-        System.out.println("Categories have been seeded:");
-        categories.forEach(System.out::println);
-    }
+                System.out.println("Categories have been seeded:");
+                categories.forEach(System.out::println);
+        }
 }
--- servicemarket/src/main/java/servicemarket/servicemarket/security/SecurityConfig.java
+++ servicemarket/src/main/java/servicemarket/servicemarket/security/SecurityConfig.java
@@ -20,57 +20,57 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private JwtAuthTokenFilter jwtAuthTokenFilter;
+        @Autowired
+        private JwtAuthTokenFilter jwtAuthTokenFilter;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**",
-                                "/swagger-ui/index.html")
-                        .permitAll()
-                        .requestMatchers("/categories/page",
-                                "/tasks/offers/page",
-                                "/tasks/requests/page",
-                                "/tasks/requests/category/*/page",
-                                "/tasks/offers/category/*/page",
-                                "/tasks/image/*/*",
-                                "/tasks/*")
-                        .permitAll()
-                        .anyRequest().authenticated())
-                .sessionManagement(management -> management
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http.csrf(csrf -> csrf.disable())
+                                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                                .authorizeHttpRequests(requests -> requests
+                                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**",
+                                                                "/swagger-ui/index.html")
+                                                .permitAll()
+                                                .requestMatchers("/categories/page",
+                                                                "/tasks/offers/page",
+                                                                "/tasks/requests/page",
+                                                                "/tasks/requests/category/*/page",
+                                                                "/tasks/offers/category/*/page",
+                                                                "/tasks/image/*/*",
+                                                                "/tasks/*")
+                                                .permitAll()
+                                                .anyRequest().authenticated())
+                                .sessionManagement(management -> management
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .addFilterBefore(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
+        @Bean
+        CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:80",
-                "http://localhost:3000",
-                "http://frontend-service:80",
-                "http://localhost:8085",
-                "http://user-service:8085"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept"));
-        configuration.setAllowCredentials(true);
+                configuration.setAllowedOrigins(List.of(
+                                "http://localhost:80",
+                                "http://localhost:3000",
+                                "http://frontend-service:80",
+                                "http://localhost:8085",
+                                "http://user-service:8085"));
+                configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept"));
+                configuration.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
 
-        return source;
-    }
+                return source;
+        }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+                        throws Exception {
+                return authenticationConfiguration.getAuthenticationManager();
+        }
 
 }
